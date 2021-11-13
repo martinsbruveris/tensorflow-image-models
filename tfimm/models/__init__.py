13ec46eb02b896619b593df36a96ea84ca0d8cf0
Martins Bruveris <martins.bruveris@onfido.com>
2021-11-13 10:04:23 +0000

    Added logging.

--- tfimm/models/__init__.py
+++ tfimm/models/__init__.py
@@ -1,2 +1,2 @@
 from .config import ModelConfig  # noqa: F401
-from .registry import register_model  # noqa: F401
+from .registry import is_model, register_model  # noqa: F401
--- tfimm/utils/__init__.py
+++ tfimm/utils/__init__.py
@@ -1,2 +1,3 @@
 from .etc import to_2tuple  # noqa: F401
+from .logging import setup_logging  # noqa: F401
 from .timm import load_timm_weights  # noqa: F401
