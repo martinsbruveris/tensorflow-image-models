e721c32a1ea79a09920453b0d4c5c8795ae97019
Martins Bruveris <martins.bruveris@onfido.com>
2021-11-13 23:07:31 +0000

    First version of ViT.

--- tfimm/layers/factory.py
+++ tfimm/layers/factory.py
@@ -3,10 +3,8 @@ import tensorflow as tf
 
 def act_layer_factory(act_layer: str):
     """Returns a function that creates the required activation layer."""
-    if act_layer == "swish":
-        return lambda: tf.keras.layers.Activation("swish")
-    elif act_layer == "relu":
-        return lambda: tf.keras.layers.Activation("relu")
+    if act_layer in {"swish", "relu", "gelu"}:
+        return lambda: tf.keras.layers.Activation(act_layer)
     else:
         raise ValueError(f"Unknown activation: {act_layer}.")
 
