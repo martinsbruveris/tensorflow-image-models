3afa3372d122e8b76eefc3ded24ed8cf3e670ffa
Martins Bruveris <martins.bruveris@gmx.at>
2021-11-16 14:26:38 +0200

    Added missing vit models.

--- tfimm/utils/__init__.py
+++ tfimm/utils/__init__.py
@@ -1,3 +1,3 @@
-from .constants import *
+from .constants import *  # noqa: F401
 from .etc import to_2tuple  # noqa: F401
 from .timm import load_timm_weights  # noqa: F401
--- tfimm/utils/constants.py
+++ tfimm/utils/constants.py
@@ -1,3 +1,5 @@
+# Note that mean and std have to be multiplied by 255 before being applied to pixels.
+# See PrefetchLoader class in timm/data/loader.py
 IMAGENET_DEFAULT_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_DEFAULT_STD = (0.229, 0.224, 0.225)
 IMAGENET_INCEPTION_MEAN = (0.5, 0.5, 0.5)
