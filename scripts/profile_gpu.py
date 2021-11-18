c467e7a39371045a35344fd7e98679e6d5f937f4
Martins Bruveris <martins.bruveris@gmx.at>
2021-11-19 01:22:59 +0200

    Fixed bug.

--- scripts/profile_gpu.py
+++ scripts/profile_gpu.py
@@ -44,17 +44,17 @@ def main(results_file, name_filter, module, exclude_filters, ignore_results):
         results_df.loc[model_name, "image_size"] = cfg.input_size[0]
 
         batch_size = find_max_batch_size(
-            model_name, test_target="inference", verbose=True
+            model_name, test_target="inference", verbose=False
         )
-        img_per_sec = time_backprop(model_name, batch_size, nb_batches=3)
+        img_per_sec = time_inference(model_name, batch_size, nb_batches=3)
         results_df.loc[model_name, "inference_batch_size"] = batch_size
         results_df.loc[model_name, "infererence_img_per_sec"] = img_per_sec
         print(f"Inference: {img_per_sec:.3f}img/sec with {batch_size} batch size.")
 
         batch_size = find_max_batch_size(
-            model_name, test_target="backprop", verbose=True
+            model_name, test_target="backprop", verbose=False
         )
-        img_per_sec = time_inference(model_name, batch_size, nb_batches=3)
+        img_per_sec = time_backprop(model_name, batch_size, nb_batches=3)
         results_df.loc[model_name, "backprop_batch_size"] = batch_size
         results_df.loc[model_name, "backprop_img_per_sec"] = img_per_sec
         print(f"Backprop: {img_per_sec:.3f}img/sec with {batch_size} batch size.")
