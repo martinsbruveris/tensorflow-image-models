d3ccee9af3c4280680c541b4a0ef7851613b40cf
Martins Bruveris <martins.bruveris@gmx.at>
2021-12-06 16:35:51 +0000

    Added blur pool.

--- tfimm/layers/__init__.py
+++ tfimm/layers/__init__.py
@@ -1,4 +1,5 @@
 from .attention import EcaModule, attn_layer_factory  # noqa: F401
+from .blurpool import BlurPool2D  # noqa: F401
 from .classifier import ClassifierHead  # noqa: F401
 from .drop import DropPath  # noqa: F401
 from .factory import act_layer_factory, norm_layer_factory  # noqa: F401
