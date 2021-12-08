fdd95620ccb29ce961e833e10407d6326f5b1111
Martins Bruveris <martins.bruveris@gmx.at>
2021-12-08 09:41:10 +0000

    Added pth weight loading.

--- tfimm/utils/__init__.py
+++ tfimm/utils/__init__.py
@@ -1,3 +1,3 @@
 from .constants import *  # noqa: F401
 from .etc import to_2tuple  # noqa: F401
-from .timm import load_timm_weights  # noqa: F401
+from .timm import load_pth_url_weights, load_timm_weights  # noqa: F401
