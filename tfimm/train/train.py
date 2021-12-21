4a9be433d3d57d3dc5df122dbbc0a3be515606e7
gietema <jochem.gietema@onfido.com>
2021-12-21 12:23:37 +0000

    Revert order

--- tfimm/train/train.py
+++ tfimm/train/train.py
@@ -25,8 +25,8 @@ class ExperimentConfig:
     # Data
     train_dataset: Any
     train_dataset_class: str
-    val_dataset_class: str = ""
     val_dataset: Any = None
+    val_dataset_class: str = ""
     # One of 0, 10, 20, 30, 40, 50: NOTSET, DEBUG, INFO, WARNING, ERROR, CRITICAL
     logging_level: int = 30
     # W&B parameters
