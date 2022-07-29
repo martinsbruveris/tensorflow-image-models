f0d0a94a8066f7407f824e1761b86c4a879a1873
Marian <marian.schnei@gmail.com>
2022-07-29 16:43:53 +0100

    Delete SpectralNormalization class.

--- tfimm/layers/spectral_normalization.py
+++ tfimm/layers/spectral_normalization.py
@@ -10,7 +10,7 @@
     Regularisation of neural networks by enforcing lipschitz continuity.
     _arXiv preprint arXiv:1804.04368_, 2018. https://arxiv.org/abs/1804.04368
 """
-# `SpectralNormalization` and  `SpectralNormalizationConv2D` classes copied from
+# `SpectralNormalizationConv2D` class copied from
 # `https://github.com/tensorflow/models/blob/master/official/nlp/modeling/layers/spectral_normalization.py`
 #
 # Copyright 2022 The TensorFlow Authors. All Rights Reserved.
@@ -27,136 +27,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-import numpy as np
 import tensorflow as tf
-
-
-class SpectralNormalization(tf.keras.layers.Wrapper):
-    """Implements spectral normalization for Dense layer."""
-
-    def __init__(
-        self,
-        layer,
-        iteration=1,
-        norm_multiplier=0.95,
-        training=True,
-        aggregation=tf.VariableAggregation.MEAN,
-        inhere_layer_name=False,
-        **kwargs,
-    ):
-        """Initializer.
-        Args:
-          layer: (tf.keras.layers.Layer) A TF Keras layer to apply normalization to.
-          iteration: (int) The number of power iteration to perform to estimate
-            weight matrix's singular value.
-          norm_multiplier: (float) Multiplicative constant to threshold the
-            normalization. Usually under normalization, the singular value will
-            converge to this value.
-          training: (bool) Whether to perform power iteration to update the singular
-            value estimate.
-          aggregation: (tf.VariableAggregation) Indicates how a distributed variable
-            will be aggregated. Accepted values are constants defined in the class
-            tf.VariableAggregation.
-          inhere_layer_name: (bool) Whether to inhere the name of the input layer.
-          **kwargs: (dict) Other keyword arguments for the layers.Wrapper class.
-        """
-        self.iteration = iteration
-        self.do_power_iteration = training
-        self.aggregation = aggregation
-        self.norm_multiplier = norm_multiplier
-
-        # Set layer name.
-        wrapper_name = kwargs.pop("name", None)
-        if inhere_layer_name:
-            wrapper_name = layer.name
-
-        if not isinstance(layer, tf.keras.layers.Layer):
-            raise ValueError(
-                f"`layer` must be a `tf.keras.layer.Layer`. Observed `{layer}`"
-            )
-        super().__init__(layer, name=wrapper_name, **kwargs)
-
-    def build(self, input_shape):
-        super().build(input_shape)
-        self.layer.kernel._aggregation = (
-            self.aggregation
-        )  # pylint: disable=protected-access
-        self._dtype = self.layer.kernel.dtype
-
-        self.w = self.layer.kernel
-        self.w_shape = self.w.shape.as_list()
-
-        self.v = self.add_weight(
-            shape=(1, np.prod(self.w_shape[:-1])),
-            initializer=tf.initializers.random_normal(),
-            trainable=False,
-            name="v",
-            dtype=self.dtype,
-            aggregation=self.aggregation,
-        )
-
-        self.u = self.add_weight(
-            shape=(1, self.w_shape[-1]),
-            initializer=tf.initializers.random_normal(),
-            trainable=False,
-            name="u",
-            dtype=self.dtype,
-            aggregation=self.aggregation,
-        )
-
-        self.update_weights()
-
-    def call(self, inputs, *, training=None):
-        training = self.do_power_iteration if training is None else training
-        if training:
-            u_update_op, v_update_op, w_update_op = self.update_weights(
-                training=training
-            )
-            output = self.layer(inputs)
-            w_restore_op = self.restore_weights()
-
-            # Register update ops.
-            self.add_update(u_update_op)
-            self.add_update(v_update_op)
-            self.add_update(w_update_op)
-            self.add_update(w_restore_op)
-        else:
-            output = self.layer(inputs)
-
-        return output
-
-    def update_weights(self, *, training=True):
-        w_reshaped = tf.reshape(self.w, [-1, self.w_shape[-1]])
-
-        u_hat = self.u
-        v_hat = self.v
-
-        if training:
-            for _ in range(self.iteration):
-                v_hat = tf.nn.l2_normalize(tf.matmul(u_hat, tf.transpose(w_reshaped)))
-                u_hat = tf.nn.l2_normalize(tf.matmul(v_hat, w_reshaped))
-
-        sigma = tf.matmul(tf.matmul(v_hat, w_reshaped), tf.transpose(u_hat))
-        # Convert sigma from a 1x1 matrix to a scalar.
-        sigma = tf.reshape(sigma, [])
-        u_update_op = self.u.assign(u_hat)
-        v_update_op = self.v.assign(v_hat)
-
-        # Bound spectral norm to be not larger than self.norm_multiplier.
-        w_norm = tf.cond(
-            (self.norm_multiplier / sigma) < 1,
-            lambda: (self.norm_multiplier / sigma)  # pylint:disable=g-long-lambda
-            * self.w,
-            lambda: self.w,
-        )
-
-        w_update_op = self.layer.kernel.assign(w_norm)
-        return u_update_op, v_update_op, w_update_op
-
-    def restore_weights(self):
-        """Restores layer weights to maintain gradient update (See Alg 1 of [1])."""
-        return self.layer.kernel.assign(self.w)
 
 
 class SpectralNormalizationConv2D(tf.keras.layers.Wrapper):
