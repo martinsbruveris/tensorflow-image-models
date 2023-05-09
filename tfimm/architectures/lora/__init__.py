7b2f660ed8a9fa119c3d4426035755130af8bdb1
Martins Bruveris <martins.bruveris@gmx.at>
2023-05-10 00:12:39 +0100

    Fixing things.:

--- tfimm/architectures/lora/__init__.py
+++ tfimm/architectures/lora/__init__.py
@@ -1,1 +1,3 @@
+from .convnext import *
 from .factory import create_model
+from .registry import register_lora_architecture, lora_config, lora_architecture
--- tfimm/architectures/lora/convnext.py
+++ tfimm/architectures/lora/convnext.py
@@ -6,6 +6,8 @@ from tfimm.architectures.convnext import ConvNeXt, ConvNeXtConfig
 from tfimm.models import keras_serializable
 from .registry import register_lora_architecture
 
+__all__ = ["LoRAConvNeXt", "LoRAConvNeXtConfig"]
+
 
 # TODO: This is temporary...
 class LoraDense(tf.keras.layers.Dense):
@@ -14,12 +16,14 @@ class LoraDense(tf.keras.layers.Dense):
         self.lora_weight = None
 
     def build(self, input_shape):
+        super().build(input_shape)
         self.lora_weight = self.add_weight(
             name="lora_weight",
             shape=(1,),
             initializer=tf.keras.initializers.constant(0.0),
             trainable=True,
         )
+
 
     def call(self, x):
         return super().call(x + self.lora_weight)
@@ -28,7 +32,7 @@ class LoraDense(tf.keras.layers.Dense):
 @dataclass
 class LoRAConvNeXtConfig(ConvNeXtConfig):
     lora_rank: int = 2
-    lora_alpha: float = 1.0  # TODO: Find better default value
+    lora_alpha: float = 1.0
 
 
 
@@ -36,6 +40,10 @@ class LoRAConvNeXtConfig(ConvNeXtConfig):
 @register_lora_architecture
 class LoRAConvNeXt(ConvNeXt):
 
+    # TODO (martins): I should rewrite the `transfer_weights` function to iterate over
+    #   src weights instead of dst weights. In that case we wouldn't need to add
+    #   lora_weights here.
+    keys_to_ignore_on_load_missing = ["lora_weight"]
     cfg_class = LoRAConvNeXtConfig
 
     def __init__(self, cfg: LoRAConvNeXtConfig, **kwargs):
