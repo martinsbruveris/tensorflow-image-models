a67a7b9645672413982e7997ba7dc6db6dc94869
Martins Bruveris <martins.bruveris@gmx.at>
2023-05-10 13:20:36 +0100

    Update __init__.

--- tfimm/architectures/lora/__init__.py
+++ tfimm/architectures/lora/__init__.py
@@ -1,5 +1,9 @@
 from .convnext import *  # noqa: F401
-from .factory import create_model, mark_only_lora_as_trainable  # noqa: F401
+from .factory import (  # noqa: F401
+    convert_to_lora_model,
+    create_model,
+    set_only_lora_layers_trainable,
+)
 from .registry import (  # noqa: F401
     lora_architecture,
     lora_config,
