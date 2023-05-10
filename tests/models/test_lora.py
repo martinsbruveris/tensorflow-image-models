3a32c4e82ad40945511d8b41aa83fa266557b753
Martins Bruveris <martins.bruveris@gmx.at>
2023-05-10 16:35:11 +0100

    Improved test.

--- tests/models/test_lora.py
+++ tests/models/test_lora.py
@@ -1,13 +1,14 @@
-
+import pytest
 import tensorflow as tf
 
 from tfimm.architectures import lora
 
-
-def test_lora_dense():
+# We test both rank 2 and rank 3 inputs, since they use different paths in call().
+@pytest.mark.parametrize("input_shape", [(3, 4), (3, 4, 5)])
+def test_lora_dense(input_shape):
     """Creating the layer and running inference in merged and non-merged modes."""
     layer = lora.LoRADense(units=2, use_bias=True, activation="swish")
-    layer.build(input_shape=(3, 4))
+    layer.build(input_shape=input_shape)
     assert not layer.merged
 
     # Set non-trivial weights, since bias and kernel_b are zero-initialised.
@@ -15,7 +16,7 @@ def test_lora_dense():
     layer.kernel_lora_a = tf.random.uniform(layer.kernel_lora_a.shape)
     layer.kernel_lora_b = tf.random.uniform(layer.kernel_lora_b.shape)
 
-    x = tf.random.uniform((3, 4))
+    x = tf.random.uniform(input_shape)
     res_1 = layer(x)
 
     layer.merge_weights()
